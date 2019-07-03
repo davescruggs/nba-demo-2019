82efcffa5a7f8f16e1d1b901d36db5f6eb2b373b
David Scruggs <dscruggs@dscruggs-ltm.internal.salesforce.com>
2019-07-03 14:51:20 -0400

    package install script

--- installPackages.sh
+++ installPackages.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/sh 
 sfdx force:package:install -s AllUsers -p 04t0b000001jiGJAAY -r -w 8
-sfdx force:package:install -s AllUsers -p 04t1H000000X8jZQAS -r -w 3  
+sfdx force:package:install -s AllUsers -p 04t1H000000X8jZQAS -r -w 8  
